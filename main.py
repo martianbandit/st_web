4104e992163da1e95258d4c4a43285775bdb9369
touyamasan <stampedexam@gmail.com>
2024-05-09 00:59:51 +0900

    3rd commit

--- main.py
+++ main.py
@@ -1,63 +1,353 @@
-#import os
-
-#リストファイルを削除
-#os.remove('list.txt')
 
 import streamlit as st
 import pickle
+import datetime
+import time
+
+import streamlit_authenticator as stauth
+import yaml
 
 
-f = open('./list.txt','rb')
-list0 = pickle.load(f)
-list1 = pickle.load(f)
-list2 = pickle.load(f)
-list3 = pickle.load(f)
-list4 = pickle.load(f)
-list5 = pickle.load(f)
-list6 = pickle.load(f)
-list7 = pickle.load(f)
-list8 = pickle.load(f)
+DIFF_JST_FROM_UTC = 9
+ent_time = datetime.datetime.utcnow() + datetime.timedelta(hours=DIFF_JST_FROM_UTC)
+
+start = time.time() 
+
+with open('config.yaml') as file:
+    config = yaml.load(file, Loader=yaml.SafeLoader)
+
+authenticator = stauth.Authenticate(
+    config['credentials'],
+    config['cookie']['name'],
+    config['cookie']['key'],
+    config['cookie']['expiry_days'],
+    config['preauthorized'],
+)
+
+name, authentication_status, username = authenticator.login('main', 'main')
 
 
-st.title(f"{list0[0]}/{list0[1]}")
-st.title(f"{list0[2]}:{list0[3]} 登録")
+if 'authentication_status' not in st.session_state:
+    st.session_state['authentication_status'] = None
 
-st.header('1号')
-st.subheader(f"  A: {list1[0]}")
-st.subheader(f"  B: {list1[1]}")
-st.subheader(f"  C: {list1[2]}")
+if st.session_state["authentication_status"]:
+    authenticator.logout('Logout', 'main')
+    st.write(f'ログインに成功しました')
+		
+  
+  
+        #ログイン後の処理
 
-st.header('2号')
-st.subheader(f"  A: {list2[0]}")
-st.subheader(f"  B: {list2[1]}")
-st.subheader(f"  C: {list2[2]}")
+    with st.form(key="1_form"): 
 
-st.header('3号')
-st.subheader(f"  A: {list3[0]}")
-st.subheader(f"  B: {list3[1]}")
-st.subheader(f"  C: {list3[2]}")
+        st.subheader('1号')
+        #テキストボックス
+        tank1A= st.text_input(('1-A'),0)
+        a1= st.checkbox("1a")
+        tank1B= st.text_input(('1-B'),0)
+        b1= st.checkbox("1b")
+        tank1C= st.text_input(('1-C'),0)
+        c1= st.checkbox("1c")
+                
+        st.subheader('2号')
+        #テキストボックス
+        tank2A= st.text_input(('2-A'),0)
+        a2= st.checkbox("2a")
+        tank2B= st.text_input(('2-B'),0)
+        b2= st.checkbox("2b")
+        tank2C= st.text_input(('2-C'),0)
+        c2= st.checkbox("2c")
+                
+        st.subheader('3号')
+        #テキストボックス
+        tank3A= st.text_input(('3-A'),0)
+        a3= st.checkbox("3a")
+        tank3B= st.text_input(('3-B'),0)
+        b3= st.checkbox("3b")
+        tank3C= st.text_input(('3-C'),0)
+        c3= st.checkbox("3c")
 
-st.header('4号')
-st.subheader(f"  A: {list4[0]}")
-st.subheader(f"  B: {list4[1]}")
-st.subheader(f"  C: {list4[2]}")
+        st.subheader('4号')
+        #テキストボックス
+        tank4A= st.text_input(('4-A'),0)
+        a4= st.checkbox("4a")
+        tank4B= st.text_input(('4-B'),0)
+        b4= st.checkbox("4b")
+        tank4C= st.text_input(('4-C'),0)
+        c4= st.checkbox("4c")
+                
+        st.subheader('5号')
+        #テキストボックス
+        tank5A= st.text_input(('5-A'),0)
+        a5= st.checkbox("5a")
+        tank5B= st.text_input(('5-B'),0)
+        b5= st.checkbox("5b")
+        tank5C= st.text_input(('5-C'),0)
+        c5= st.checkbox("5c")
+                
+        st.subheader('6号')
+        #テキストボックス
+        tank6A= st.text_input(('6-A'),0)
+        a6= st.checkbox("6a")
+        tank6B= st.text_input(('6-B'),0)
+        b6= st.checkbox("6b")
+        tank6C= st.text_input(('6-C'),0)
+        c6= st.checkbox("6c")
+                
+        st.subheader('7号')
+        #テキストボックス
+        tank7A= st.text_input(('7-A'),0)
+        a7= st.checkbox("7a")
+        tank7B= st.text_input(('7-B'),0)
+        b7= st.checkbox("7b")
+        tank7C= st.text_input(('7-C'),0)       
+        c7= st.checkbox("7c")
+                
+        st.subheader('8号')
+        #テキストボックス
+        tank8A= st.text_input(('8-A'),0)
+        a8= st.checkbox("8a")
+        tank8B= st.text_input(('8-B'),0)
+        b8= st.checkbox("8b")
+        tank8C= st.text_input(('8-C'),0)
+        c8= st.checkbox("8c")
+        
+        consumption_rate = st.number_input('【------消費率------】',0.8) 
+        hour_later = st.number_input('時間',0)
+        
+        submit_btn = st.form_submit_button("送信")
+        cancel_btn = st.form_submit_button("キャンセル")
 
-st.header('5号')
-st.subheader(f"  A: {list5[0]}")
-st.subheader(f"  B: {list5[1]}")
-st.subheader(f"  C: {list5[2]}")
 
-st.header('6号')
-st.subheader(f"  A: {list6[0]}")
-st.subheader(f"  B: {list6[1]}")
-st.subheader(f"  C: {list6[2]}")
 
-st.header('7号')
-st.subheader(f"  A: {list7[0]}")
-st.subheader(f"  B: {list7[1]}")
-st.subheader(f"  C: {list7[2]}")
+    if submit_btn:
+                
+        #リストファイルをつくる
+        f = open('list.txt','wb')
+        
+        list_start = []
+        list_start.append(float(start))
+                    
+        list0 = []
+        #list0.append(format(ent_time,'%Y/%m/%d'))
+        #list0.append(format(ent_time,'%H:%M'))
+        list0.append(format(ent_time,'%Y'))
+        list0.append(format(ent_time,'%m'))
+        list0.append(format(ent_time,'%d'))
+        list0.append(format(ent_time,'%H'))
+        list0.append(format(ent_time,'%M'))
+                              
+        list1 = []
+        list1.append(float(tank1A))
+        list1.append(float(tank1B))
+        list1.append(float(tank1C))
+                    
+        list2 = []
+        list2.append(float(tank2A))
+        list2.append(float(tank2B))
+        list2.append(float(tank2C))
+                    
+        list3 = []
+        list3.append(float(tank3A))
+        list3.append(float(tank3B))
+        list3.append(float(tank3C))
+                    
+        list4 = []
+        list4.append(float(tank4A))
+        list4.append(float(tank4B))
+        list4.append(float(tank4C))
 
-st.header('8号')
-st.subheader(f"  A: {list8[0]}")
-st.subheader(f"  B: {list8[1]}")
-st.subheader(f"  C: {list8[2]}")
+        list5 = []
+        list5.append(float(tank5A))
+        list5.append(float(tank5B))
+        list5.append(float(tank5C))
+                    
+        list6 = []
+        list6.append(float(tank6A))
+        list6.append(float(tank6B))
+        list6.append(float(tank6C))
+                    
+        list7 = []
+        list7.append(float(tank7A))
+        list7.append(float(tank7B))
+        list7.append(float(tank7C))
+                        
+        list8 = []
+        list8.append(float(tank8A))
+        list8.append(float(tank8B))
+        list8.append(float(tank8C))
+        
+        consumption_list = []
+        consumption_list.append(float(consumption_rate))
+        hour_later_list = []
+        hour_later_list.append(int(hour_later))  
+                  
+        #リストファイルを保存 
+        pickle.dump(list0,f)   
+        pickle.dump(list1,f)
+        pickle.dump(list2,f)
+        pickle.dump(list3,f)
+        pickle.dump(list4,f)
+        pickle.dump(list5,f)
+        pickle.dump(list6,f)
+        pickle.dump(list7,f)
+        pickle.dump(list8,f)
+        pickle.dump(consumption_list,f)
+        pickle.dump(hour_later_list,f)
+        
+        
+        v1a = []
+        v1b = []
+        v1c = []
+        v2a = []
+        v2b = []
+        v2c = []
+        v3a = []
+        v3b = []
+        v3c = []
+        v4a = []
+        v4b = []
+        v4c = []
+        v5a = []
+        v5b = []
+        v5c = []
+        v6a = []
+        v6b = []
+        v6c = []
+        v7a = []
+        v7b = []
+        v7c = []
+        v8a = []
+        v8b = []
+        v8c = []
+        
+
+        if a1:
+            v1a.append('　←開')
+        else:
+            v1a.append('')
+        if b1:
+            v1b.append('　←開')
+        else:
+            v1b.append('')
+        if c1:
+            v1c.append('　←開')
+        else:
+            v1c.append('')   
+        if a2:
+            v2a.append('　←開')
+        else:
+            v2a.append('')
+        if b2:
+            v2b.append('　←開')
+        else:
+            v2b.append('')
+        if c2:
+            v2c.append('　←開')
+        else:
+            v2c.append('')  
+        if a3:
+            v3a.append('　←開')
+        else:
+            v3a.append('')
+        if b3:
+            v3b.append('　←開')
+        else:
+            v3b.append('')
+        if c3:
+            v3c.append('　←開')
+        else:
+            v3c.append('')         
+        if a4:
+            v4a.append('　←開')
+        else:
+            v4a.append('')
+        if b4:
+            v4b.append('　←開')
+        else:
+            v4b.append('')
+        if c4:
+            v4c.append('　←開')
+        else:
+            v4c.append('')         
+        if a5:
+            v5a.append('　←開')
+        else:
+            v5a.append('')
+        if b5:
+            v5b.append('　←開')
+        else:
+            v5b.append('')
+        if c5:
+            v5c.append('　←開')
+        else:
+            v5c.append('')         
+        if a6:
+            v6a.append('　←開')
+        else:
+            v6a.append('')
+        if b6:
+            v6b.append('　←開')
+        else:
+            v6b.append('')
+        if c6:
+            v6c.append('　←開')
+        else:
+            v6c.append('')         
+        if a7:
+            v7a.append('　←開')
+        else:
+            v7a.append('')
+        if b7:
+            v7b.append('　←開')
+        else:
+            v7b.append('')
+        if c7:
+            v7c.append('　←開')
+        else:
+            v7c.append('')         
+        if a8:
+            v8a.append('　←開')
+        else:
+            v8a.append('')
+        if b8:
+            v8b.append('　←開')
+        else:
+            v8b.append('')
+        if c8:
+            v8c.append('　←開')
+        else:
+            v8c.append('')         
+            
+        pickle.dump(v1a,f)
+        pickle.dump(v1b,f)
+        pickle.dump(v1c,f)
+        pickle.dump(v2a,f)
+        pickle.dump(v2b,f)
+        pickle.dump(v2c,f)
+        pickle.dump(v3a,f)
+        pickle.dump(v3b,f)
+        pickle.dump(v3c,f)
+        pickle.dump(v4a,f)
+        pickle.dump(v4b,f)
+        pickle.dump(v4c,f)
+        pickle.dump(v5a,f)
+        pickle.dump(v5b,f)
+        pickle.dump(v5c,f)
+        pickle.dump(v6a,f)
+        pickle.dump(v6b,f)
+        pickle.dump(v6c,f)
+        pickle.dump(v7a,f)
+        pickle.dump(v7b,f)
+        pickle.dump(v7c,f)
+        pickle.dump(v8a,f)
+        pickle.dump(v8b,f)
+        pickle.dump(v8c,f)   
+        
+        pickle.dump(list_start,f)
+        
+        ####
+elif st.session_state["authentication_status"] is False:
+    st.error('ユーザ名またはパスワードが間違っています')
+elif st.session_state["authentication_status"] is None:
+    st.warning('ユーザ名やパスワードを入力してください') 
---
+++ pages/dispay.py
@@ -0,0 +1,316 @@
+#import os
+
+#リストファイルを削除
+#os.remove('list.txt')
+
+import streamlit as st
+import pickle
+import datetime
+import time
+
+DIFF_JST_FROM_UTC = 9
+now = datetime.datetime.utcnow() + datetime.timedelta(hours=DIFF_JST_FROM_UTC)
+
+ut_now = time.time()
+
+f = open('./list.txt','rb')
+
+list0 = pickle.load(f)
+list1 = pickle.load(f)
+list2 = pickle.load(f)
+list3 = pickle.load(f)
+list4 = pickle.load(f)
+list5 = pickle.load(f)
+list6 = pickle.load(f)
+list7 = pickle.load(f)
+list8 = pickle.load(f)
+
+consumption_list = pickle.load(f)
+hour_later_list =pickle.load(f)
+
+v1a = pickle.load(f)
+v1b = pickle.load(f)
+v1c = pickle.load(f)
+v2a = pickle.load(f)
+v2b = pickle.load(f)
+v2c = pickle.load(f)
+v3a = pickle.load(f)
+v3b = pickle.load(f)
+v3c = pickle.load(f)
+v4a = pickle.load(f)
+v4b = pickle.load(f)
+v4c = pickle.load(f)
+v5a = pickle.load(f)
+v5b = pickle.load(f)
+v5c = pickle.load(f)
+v6a = pickle.load(f)
+v6b = pickle.load(f)
+v6c = pickle.load(f)
+v7a = pickle.load(f)
+v7b = pickle.load(f)
+v7c = pickle.load(f)
+v8a = pickle.load(f)
+v8b = pickle.load(f)
+v8c = pickle.load(f)
+
+list_start = pickle.load(f)
+
+    
+
+#登録時間
+start = list_start[0]
+#消費率
+consumption_rate = consumption_list[0]
+#x時間後
+hour_later = hour_later_list[0]
+#x時間後の日時
+later_ = datetime.datetime.utcnow() + datetime.timedelta(hours=DIFF_JST_FROM_UTC + hour_later_list[0])
+#経過時間
+progress = ((ut_now - start)/3600 )
+#消費量
+consumption = consumption_rate * (progress+hour_later)
+
+
+entY = list0[0]
+entm = list0[1]
+entd = list0[2]
+entH = list0[3]
+entM = list0[4]
+
+
+
+
+
+
+
+if hour_later >=20 :
+    list1[0,0,0]
+    list2[0,0,0]
+    list3[0,0,0]
+    list4[0,0,0]
+    list5[0,0,0]
+    list6[0,0,0]
+    list7[0,0,0]
+    list8[0,0,0]
+
+#######
+if any(v1a):
+    lv1a = (list1[0] - consumption)
+else:
+    lv1a = list1[0]
+lv1a = round(lv1a,2)   
+if any(v1b):
+    lv1b = (list1[1] - consumption)
+else:
+    lv1b = list1[1]
+lv1b = round(lv1b,2) 
+if any(v1c):
+    lv1c = (list1[2] - consumption)
+else:
+    lv1c = list1[2]
+lv1c = round(lv1c,2)
+
+
+if any(v2a):
+    lv2a = (list2[0] - consumption)
+else:
+    lv2a = list2[0]
+lv2a = round(lv2a,2)   
+if any(v2b):
+    lv2b = (list2[1] - consumption)
+else:
+    lv2b = list2[1]
+lv2b = round(lv2b,2) 
+if any(v2c):
+    lv2c = (list2[2] - consumption)
+else:
+    lv2c = list2[2]
+lv2c = round(lv2c,2)
+
+if any(v3a):
+    lv3a = (list3[0] - consumption)
+else:
+    lv3a = list3[0]
+lv3a = round(lv3a,2)   
+if any(v3b):
+    lv3b = (list3[1] - consumption)
+else:
+    lv3b = list3[1]
+lv3b = round(lv3b,2) 
+if any(v3c):
+    lv3c = (list3[2] - consumption)
+else:
+    lv3c = list3[2]
+lv3c = round(lv3c,2)  
+
+if any(v4a):
+    lv4a = (list4[0] - consumption)
+else:
+    lv4a = list4[0]
+lv4a = round(lv4a,2)   
+if any(v4b):
+    lv4b = (list4[1] - consumption)
+else:
+    lv4b = list4[1]
+lv4b = round(lv4b,2) 
+if any(v4c):
+    lv4c = (list4[2] - consumption)
+else:
+    lv4c = list4[2]
+lv4c = round(lv4c,2)
+
+if any(v5a):
+    lv5a = (list5[0] - consumption)
+else:
+    lv5a = list5[0]
+lv5a = round(lv5a,2)   
+if any(v1b):
+    lv5b = (list5[1] - consumption)
+else:
+    lv5b = list5[1]
+lv5b = round(lv5b,2) 
+if any(v5c):
+    lv5c = (list5[2] - consumption)
+else:
+    lv5c = list5[2]
+lv5c = round(lv5c,2)
+
+if any(v6a):
+    lv6a = (list6[0] - consumption)
+else:
+    lv6a = list6[0]
+lv6a = round(lv6a,2)   
+if any(v6b):
+    lv6b = (list6[1] - consumption)
+else:
+    lv6b = list6[1]
+lv6b = round(lv6b,2) 
+if any(v6c):
+    lv6c = (list6[2] - consumption)
+else:
+    lv6c = list6[2]
+lv6c = round(lv6c,2)
+
+if any(v7a):
+    lv7a = (list7[0] - consumption)
+else:
+    lv7a = list7[0]
+lv7a = round(lv7a,2)   
+if any(v7b):
+    lv7b = (list7[1] - consumption)
+else:
+    lv7b = list7[1]
+lv7b = round(lv7b,2) 
+if any(v7c):
+    lv7c = (list7[2] - consumption)
+else:
+    lv7c = list7[2]
+lv7c = round(lv7c,2)
+
+if any(v8a):
+    lv8a = (list8[0] - consumption)
+else:
+    lv8a = list8[0]
+lv8a = round(lv8a,2)   
+if any(v8b):
+    lv8b = (list8[1] - consumption)
+else:
+    lv8b = list8[1]
+lv8b = round(lv8b,2) 
+if any(v8c):
+    lv8c = (list8[2] - consumption)
+else:
+    lv8c = list8[2]
+lv8c = round(lv8c,2)
+######
+
+#表示
+col1, col2 = st.columns(2)
+with col1:
+    st.title(f"{list0[0]}/{list0[1]}/{list0[2]}")
+    st.title(f"{list0[3]}:{list0[4]} 登録")
+
+    st.header('---1号---')
+    st.subheader(f"A : {list1[0]}{v1a[0]}")
+    st.subheader(f"B : {list1[1]}{v1b[0]}")
+    st.subheader(f"C : {list1[2]}{v1c[0]}")
+
+    st.header('---2号---')
+    st.subheader(f"A : {list2[0]}{v2a[0]}")
+    st.subheader(f"B : {list2[1]}{v2b[0]}")
+    st.subheader(f"C : {list2[2]}{v2c[0]}")
+
+    st.header('---3号---')
+    st.subheader(f"A : {list3[0]}{v3a[0]}")
+    st.subheader(f"B : {list3[1]}{v3b[0]}")
+    st.subheader(f"C : {list3[2]}{v3c[0]}")
+
+    st.header('---4号---')
+    st.subheader(f"A : {list4[0]}{v4a[0]}")
+    st.subheader(f"B : {list4[1]}{v4b[0]}")
+    st.subheader(f"C : {list4[2]}{v4c[0]}")
+
+    st.header('---5号---')
+    st.subheader(f"A : {list5[0]}{v5a[0]}")
+    st.subheader(f"B : {list5[1]}{v5b[0]}")
+    st.subheader(f"C : {list5[2]}{v5c[0]}")
+
+    st.header('---6号---')
+    st.subheader(f"A : {list6[0]}{v6a[0]}")
+    st.subheader(f"B : {list6[1]}{v6b[0]}")
+    st.subheader(f"C : {list6[2]}{v6c[0]}")
+
+    st.header('---7号---')
+    st.subheader(f"A : {list7[0]}{v7a[0]}")
+    st.subheader(f"B : {list7[1]}{v7b[0]}")
+    st.subheader(f"C : {list7[2]}{v7c[0]}")
+
+    st.header('---8号---')
+    st.subheader(f"A : {list8[0]}{v8a[0]}")
+    st.subheader(f"B : {list8[1]}{v8b[0]}")
+    st.subheader(f"C : {list8[2]}{v8c[0]}")
+    
+with col2:
+    st.subheader(f"消費量{consumption_rate}/hとして")
+    if hour_later == 0:
+        st.subheader(f"現在時刻 {later_.strftime('%H:%M')} の")
+        st.subheader(f"予測値")
+    else:
+        st.subheader(f"今から{hour_later}時間後")
+        st.subheader(f"{later_.strftime('%H:%M')}の予測値")
+
+    st.header('---1号---')
+    st.subheader(f"A : {lv1a}")
+    st.subheader(f"B : {lv1b}")
+    st.subheader(f"C : {lv1c}")
+    st.header('---2号---')
+    st.subheader(f"A : {lv2a}")
+    st.subheader(f"B : {lv2b}")
+    st.subheader(f"C : {lv2c}")
+    st.header('---3号---')
+    st.subheader(f"A : {lv3a}")
+    st.subheader(f"B : {lv3b}")
+    st.subheader(f"C : {lv3c}")
+    st.header('---4号---')
+    st.subheader(f"A : {lv4a}")
+    st.subheader(f"B : {lv4b}")
+    st.subheader(f"C : {lv4c}")
+    st.header('--5号---')
+    st.subheader(f"A : {lv5a}")
+    st.subheader(f"B : {lv5b}")
+    st.subheader(f"C : {lv5c}")
+    st.header('---6号---')
+    st.subheader(f"A : {lv6a}")
+    st.subheader(f"B : {lv6b}")
+    st.subheader(f"C : {lv6c}")
+    st.header('---7号---')
+    st.subheader(f"A : {lv7a}")
+    st.subheader(f"B : {lv7b}")
+    st.subheader(f"C : {lv7c}")
+    st.header('---8号---')
+    st.subheader(f"A : {lv8a}")
+    st.subheader(f"B : {lv8b}")
+    st.subheader(f"C : {lv8c}")
+
+
+
